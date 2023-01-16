2b1c96064b8f8e28bf7c26e2b32a80604b19a58d
erickgnclvs <erickgoncalves5@hotmail.com>
2023-01-16 20:36:38 -0300

    Fix entry route

--- projects/2020/x/wiki/encyclopedia/views.py
+++ projects/2020/x/wiki/encyclopedia/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
+from markdown import Markdown
+
+from django.http import Http404
 
 from . import util
 
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
+    return render(request, 'encyclopedia/index.html', {
         "entries": util.list_entries()
     })
 
+def wiki(request, entry):
+    print(entry)
+    if entry not in util.list_entries():
+        return render(request, "encyclopedia/error.html", {
+            "message" : "No such entry"
+        })
+    content = util.get_entry(entry)
+    return render(request, "encyclopedia/wiki.html", {
+        "title": entry,
+        "content": Markdown().convert(content)
+        })
+
+def search(request):
+    return render(request, 'encyclopedia/search.html')
+    
